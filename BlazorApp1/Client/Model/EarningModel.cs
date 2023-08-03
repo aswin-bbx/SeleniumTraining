24148da4e8f6940a7a12ab1a8cb19817a930d43c
aswin-bbx <aswinkrishna2000@gmail.com>
2023-08-03 12:16:30 +0530

    Blazor app

---
+++ BlazorApp1/Client/Model/EarningModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Client.Model
+{
+    public class EarningModel
+    {
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Email { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public String Address { get; set; }
+        [Required]
+        public String PhoneNumber { get; set; }
+    }
+}
